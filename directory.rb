33a9aecbd94b64a7a6b54af2cb323cb6fbbff8c4
Noelle Legrain <noelle.legrain@gmail.com>
2018-02-15 13:07:40 +0000

    Added list of students

---
+++ directory.rb
@@ -0,0 +1,18 @@
+#fist we print the list of students
+puts "The students of my cohort at Makers Academy"
+puts "-------------"
+puts "Dr. Hannibal Lecter"
+puts "Darth Vader"
+puts "Nurse Ratched"
+puts "Michael Corleone"
+puts "Alex DeLarge"
+puts "The Wicked Witch of the West"
+puts "Terminator"
+puts "Freddy Krueger"
+puts "The Joker"
+puts "Joffrey Baratheon"
+puts "Norman Bates"
+#then we print the total number of students
+print "Overall, we have "
+print 11
+puts " great students"
