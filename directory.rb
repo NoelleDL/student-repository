9bc65256287c4c5720a26b8881afdd22639ac1b2
Noelle Legrain <noelle.legrain@gmail.com>
2018-02-15 15:30:06 +0000

    Added print names less than 12 characters

--- directory.rb
+++ directory.rb
@@ -24,7 +24,7 @@ def print_header
 end
 def print(students)
   students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name][0] == "N"
+    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length <= 12
   end
 end
 def print_footer(students)
