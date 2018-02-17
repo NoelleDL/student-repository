0020e69e1a345fcdd66bf9a10b1dba67d71bece3
Noelle Legrain <noelle.legrain@gmail.com>
2018-02-17 15:39:29 +0000

    Takes arguements from command line

--- directory.rb
+++ directory.rb
@@ -4,7 +4,7 @@ def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   #get the first name
-  name = gets.chop
+  name = STDIN.gets.chop
 
   #while the name is not empty, repeat this code
   while !name.empty? do
@@ -21,7 +21,7 @@ def input_students
       puts "Now we have #{@students.count} student"
     end
     #get another name from the user
-    name = gets.chop
+    name = STDIN.gets.chop
   end
   # return the array of input_student
   @students
@@ -39,6 +39,7 @@ def print_students_list
     @students.each { |student| puts student[:name] if student[:cohort].eql?(month) }
   }
 end
+
 def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
@@ -58,9 +59,9 @@ def show_students
 end
 
 def save_students
-  #open the file for writing
+  # open file for writing
   file = File.open("students.csv", "w")
-  #iterate over the array of students
+  # iterate over array of students
   @students.each do |student|
     student_data = [student[:name], student[:cohort]]
     csv_line = student_data.join(",")
@@ -69,13 +70,25 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
   name, cohort = line.chomp.split(',')
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
+end
+
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # exit if it doesn't exist
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist."
+    exit # quit the program
+  end
 end
 
 def process(selection)
@@ -98,8 +111,9 @@ end
 def interactive_menu
   loop do
     print_menu
-    process(gets.chomp)
+    process(STDIN.gets.chomp)
   end
 end
 
+try_load_students
 interactive_menu
